68110c104b6cca3a02de3158866b35eec7a9b216
Vadim Gulyakin <vadim.gulyakin@gmail.com>
2013-10-29 13:12:39 +0200

    refactored GistAPITest to conform mule-connector-test framework

---
+++ src/test/java/org/mule/modules/github/automation/testcases/gist/DeleteGistTestCases.java
@@ -0,0 +1,50 @@
+/**
+ *
+ * Mule GitHub Cloud Connector
+ *
+ * Copyright (c) MuleSoft, Inc. All rights reserved. http://www.mulesoft.com
+ * <p/>
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.md file.
+ */
+
+package org.mule.modules.github.automation.testcases.gist;
+
+import org.eclipse.egit.github.core.Gist;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.mule.modules.github.automation.testcases.GutHubTestParent;
+import org.mule.modules.github.automation.testcases.RegressionTests;
+import org.mule.modules.tests.ConnectorTestUtils;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+
+public class DeleteGistTestCases extends GutHubTestParent
+{
+    @Before
+    public void setUp() throws Exception
+    {
+        initializeTestRunMessage("gist");
+        Gist gist = runFlowAndGetPayload("createGist");
+        upsertOnTestRunMessage("gistId", gist.getId());
+    }
+
+    @Category({RegressionTests.class})
+    @Test
+    public void forkGist()
+    {
+        try
+        {
+            runFlowAndGetPayload("deleteGist");
+
+        } catch (Exception e)
+        {
+            fail(ConnectorTestUtils.getStackTrace(e));
+        }
+    }
+
+}
